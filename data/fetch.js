79550a04b789842d3a76be7de2e051a2b54b140c
YvesStraten <yves.straten@gmail.com>
2023-12-22 09:18:00 +0800

    updated js to reflect new endpoint

--- data/fetch.js
+++ data/fetch.js
@@ -2,13 +2,10 @@ const soundText = document.getElementById("sound_value");
 const pirText = document.getElementById("pir_value");
 
 setInterval(() => {
-    fetch("/data/pir").then(res => res.json()).then(data => {
+    fetch("/api/data").then(res => res.json()).then(data => {
         console.log(data);
-        pirText.textContent = data.value;
+        pirText.textContent = data.sensors.pir;
+        soundText.textContent = data.sensors.sound;
     })
 
-    fetch("/data/sound").then(res => res.json()).then(data => {
-        console.log(data);
-        soundText.textContent = data.value;
-    })
 }, 1000);
